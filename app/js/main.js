d4a67d6f3870dedb8e7baeaad67ae2f3f3504e60
SpaRGlaD <92817252+SpaRGlaD@users.noreply.github.com>
2021-11-27 12:32:11 +0200

    added full main page

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,22 @@
 $(function () {
-    
+    $('.slider-top__inner').slick({
+        dots: true,
+        arrows: false,
+        // autoplay: true,
+        autoplaySpeed: 2000,
+        fade: true,
+    })
+    var containerEl1 = document.querySelector('[data-ref="container-1"]');
+    var containerEl2 = document.querySelector('[data-ref="container-2"]');
+
+    var config = {
+        controls: {
+            scope: 'local'
+        }
+    };
+
+    var mixer1 = mixitup(containerEl1, config);
+    var mixer2 = mixitup(containerEl2, config);
+
+    // var mixer = mixitup('.product__content');
 })
